fe430c201f525d45369dfc53c5f10e4cbb74fdc3
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-01 22:54:44 +0600

    switch num switch case console break default console

---
+++ switch.js
@@ -0,0 +1,16 @@
+num = 1000
+
+switch(num ){
+    case 1000:
+        console.log('i am 1000');
+        break;
+    case 100:
+        console.log('i am 100');
+        break;
+    case 20:
+    case 15:
+        console.log('i am either 20 or 15');
+        break;
+    default:
+        console.log(`i don't know who you are`);
+}
