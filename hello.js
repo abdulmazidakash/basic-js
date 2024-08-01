2ca1906f8cf35479fd921bb8ceefa696ef1295a2
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-01 14:53:26 +0600

    learning toUpperCase toLowerCase indexof split

--- hello.js
+++ hello.js
@@ -11,21 +11,36 @@
 // console.log(mangoPrice);
 
 
-var bananaPrice = 12
-console.log(typeof bananaPrice);
+// var bananaPrice = 12
+// console.log(typeof bananaPrice);
 
-var applePrice = 120
-console.log(typeof applePrice);
+// var applePrice = 120
+// console.log(typeof applePrice);
 
-var mangoPrice = 60
-console.log(typeof mangoPrice);
+// var mangoPrice = 60
+// console.log(typeof mangoPrice);
 
-var seenAfter = 21
-console.log(typeof seenAfter);
+// var seenAfter = 21
+// console.log(typeof seenAfter);
 
-var name = "Abdul Mazid Akash"
-console.log(typeof name);
+// var name = "Abdul Mazid Akash"
+// console.log(typeof name);
 
-var isHot = true;
-var isRich = false;
-console.log(typeof isHot);
+// var isHot = true;
+// var isRich = false;
+// console.log(typeof isHot);
+
+// var promise = "I promise I will work HARD to become a programmer"
+// console.log(promise.toLowerCase());
+
+// var promise = "I promise I will work HARD to become a programmer"
+// console.log(promise.toUpperCase());
+
+// var promise = "I promise I will work HARD to become a programmer"
+// console.log(promise.indexOf("will"));
+
+// var promise = "I promise I will work HARD to become a programmer"
+// console.log(promise.split("I"));
+
+var promise = "I promise I will work HARD to become a programmer"
+console.log(promise.split(" "));
