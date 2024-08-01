370cfa5814e875350a714448f48c78890df6fa2c
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-01 22:36:28 +0600

    learning forLoop js

---
+++ forLoop.js
@@ -0,0 +1,11 @@
+// for (var i= 0; i<10; i++){
+//     console.log(i);
+// }
+
+
+var nums = [55, 66, 77, 88, 99, 11, 44];
+
+for (var i=0; i < nums.length; i++){
+    var element = nums[i];
+    console.log(element);
+}
