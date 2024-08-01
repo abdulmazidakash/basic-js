2986fdeb0308f21822cac5d8253a4dc86fc115a8
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-01 14:06:26 +0600

    variable types number string boolean

--- hello.js
+++ hello.js
@@ -1,11 +1,31 @@
 // console.log(21);
 // console.log('good morning');
 
+// var bananaPrice = 12
+// console.log(bananaPrice);
+
+// var applePrice = 120
+// console.log(applePrice);
+
+// var mangoPrice = 60
+// console.log(mangoPrice);
+
+
 var bananaPrice = 12
-console.log(bananaPrice);
+console.log(typeof bananaPrice);
 
 var applePrice = 120
-console.log(applePrice);
+console.log(typeof applePrice);
 
 var mangoPrice = 60
-console.log(mangoPrice);
+console.log(typeof mangoPrice);
+
+var seenAfter = 21
+console.log(typeof seenAfter);
+
+var name = "Abdul Mazid Akash"
+console.log(typeof name);
+
+var isHot = true;
+var isRich = false;
+console.log(typeof isHot);
