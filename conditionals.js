5f26905f7a48b4382a75e9c9781bad7992d549b7
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-01 16:51:26 +0600

    learning conditionals js if else comparison

---
+++ conditionals.js
@@ -0,0 +1,21 @@
+// var biscuitPrice = 12;
+
+// if(biscuitPrice <10){
+//     console.log("I will eat biscuit")
+// }
+// else{
+//     console.log("Khali tea is better")
+// }
+
+var picnic = 19;
+
+// if(picnic ==10){
+//     console.log("I will go to picnic")
+// }
+
+if(picnic !=10){
+    console.log("I will go to picnic")
+}
+else{
+    console.log("i will not go picnic")
+}
