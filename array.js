5c49c082a6d1ec9bec9073a7887b214cd7ed6fb4
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-01 22:00:54 +0600

    learning array shift unshift slice

--- array.js
+++ array.js
@@ -1,15 +1,34 @@
-var friendsAge = [12, 14, 17, 19];
+// var friendsAge = [12, 14, 17, 19];
 
-var sonaliAge = friendsAge [2];
+// var sonaliAge = friendsAge [2];
 
-friendsAge[1] = 21;
+// friendsAge[1] = 21;
 
-var position = friendsAge.indexOf(17);
+// var position = friendsAge.indexOf(17);
 
-console.log(friendsAge);
+// console.log(friendsAge);
 
-friendsAge.push(15);
-friendsAge.push(19);
-friendsAge.pop();
+// friendsAge.push(15);
+// friendsAge.push(19);
+// friendsAge.pop();
 
-console.log(friendsAge);
+// console.log(friendsAge);
+
+// var teaLine = ['kalam', 'salam', 'balam'];
+// console.log(teaLine);
+// teaLine.push('jalam');
+// console.log(teaLine);
+// teaLine.pop();
+// console.log(teaLine);
+
+// var teaLine = ['kalam', 'salam', 'balam']
+
+// teaLine.shift();
+// console.log(teaLine);
+// teaLine.unshift('palam');
+// console.log(teaLine);
+
+var teaLine = ['kalam', 'salam', 'balam', 'talam', 'jalam', 'malam']
+var part = teaLine.slice(2, 5)
+console.log(part);
+console.log(teaLine);
