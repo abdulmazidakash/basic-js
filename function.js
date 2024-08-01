fb4ff52b88da51acf8ef6c39111e1951e76fb5ae
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-01 23:06:09 +0600

    learning function js

---
+++ function.js
@@ -0,0 +1,12 @@
+function sayLoveYou(){
+    console.log('jaan o baby');
+    console.log('billy baby... koi tumi');
+}
+
+sayLoveYou();
+
+var date = 14;
+var place = 'restaurent';
+sayLoveYou();
+var hangoutPlace = 'tsc';
+sayLoveYou();
