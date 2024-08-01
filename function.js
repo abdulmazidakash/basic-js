248120e7a3b5ae293c29a792c2e9de0c899ea55e
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-02 00:16:06 +0600

    function js

--- function.js
+++ function.js
@@ -1,12 +1,38 @@
-function sayLoveYou(){
-    console.log('jaan o baby');
-    console.log('billy baby... koi tumi');
+// function sayLoveYou(){
+//     console.log('jaan o baby');
+//     console.log('billy baby... koi tumi');
+// }
+
+// sayLoveYou();
+
+// var date = 14;
+// var place = 'restaurent';
+// sayLoveYou();
+// var hangoutPlace = 'tsc';
+// sayLoveYou();
+
+// function doubleIt (num){
+//     var result= num *2;
+//     console.log(result);
+// }
+
+// doubleIt(5);
+// doubleIt(50);
+// doubleIt(500);
+
+function doubleIt (num){
+    var result = num *2;
+    return result;
+}
+var first = doubleIt(5);
+var second = doubleIt(50);
+var total = first + second;
+// console.log(total);
+
+function add(num1, num2){
+    var result = num1 + num2;
+    return result;
 }
 
-sayLoveYou();
-
-var date = 14;
-var place = 'restaurent';
-sayLoveYou();
-var hangoutPlace = 'tsc';
-sayLoveYou();
+var sum = add(15, 17);
+console.log(sum);
