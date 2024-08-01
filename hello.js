87f093b7ccdcd1fcb28df8f4273c183db1232e40
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-01 13:19:33 +0600

    variable declare practice 3 times

--- hello.js
+++ hello.js
@@ -1,2 +1,11 @@
-console.log(21);
-console.log('good morning');
+// console.log(21);
+// console.log('good morning');
+
+var bananaPrice = 12
+console.log(bananaPrice);
+
+var applePrice = 120
+console.log(applePrice);
+
+var mangoPrice = 60
+console.log(mangoPrice);
