51b7bc29b87c7f93b0e6784ab8b9ae9658d69acc
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-01 15:36:32 +0600

    learning tofixed

--- hello.js
+++ hello.js
@@ -42,5 +42,34 @@
 // var promise = "I promise I will work HARD to become a programmer"
 // console.log(promise.split("I"));
 
-var promise = "I promise I will work HARD to become a programmer"
-console.log(promise.split(" "));
+// var promise = "I promise I will work HARD to become a programmer"
+// console.log(promise.split(" "));
+
+// string declare symbol : `` , "", '';
+
+// var number1 = 25;
+// var number2 = '15.5';
+// number2 = parseFloat(number2);
+// console.log(number1 + number2);
+
+// var number1 = 25;
+// var number2 = '15.5';
+// number2 = parseInt(number2);
+// console.log(number1 + number2);
+
+// var number1 = 25;
+// var number2 = '15.5';
+// number2 = +(number2);
+// console.log(number1 + number2);
+
+// var number1 = 25;
+// var number2 = 15.5;
+// number1 = " " +(number1);
+// console.log( typeof number1);
+
+var number1 = 0.1;
+var number2 = 0.2;
+var total = number1 + number2;
+// total = total.toFixed(3)
+total = total.toFixed(5)
+console.log(total);
